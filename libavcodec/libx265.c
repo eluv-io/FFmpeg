1f9ef44304c0a7de323379d88d606510ef37f646
Reza Mir <reza.mir@eluv.io>
2020-06-04 18:51:34 -0400

    H265 HDR max-cll, master-display and bframes (#21)
    * Add max-cll and master-display options to ffmpeg h265
    * Add option to set number of bframes in ffmpeg h265

--- libavcodec/libx265.c
+++ libavcodec/libx265.c
@@ -43,6 +43,9 @@ typedef struct libx265Context {
 
     float crf;
     int   forced_idr;
+    char *max_cll;
+    char *master_display;
+    int  bframes;
     char *preset;
     char *tune;
     char *profile;
@@ -216,6 +219,33 @@ static av_cold int libx265_encode_init(AVCodecContext *avctx)
     } else if (avctx->bit_rate > 0) {
         ctx->params->rc.bitrate         = avctx->bit_rate / 1000;
         ctx->params->rc.rateControlMode = X265_RC_ABR;
+    }
+
+    if (ctx->max_cll != 0) {
+        if (ctx->api->param_parse(ctx->params, "max-cll", ctx->max_cll) == X265_PARAM_BAD_VALUE) {
+            av_log(avctx, AV_LOG_ERROR, "Invalid max_cll: %s.\n", ctx->max_cll);
+            return AVERROR(EINVAL);
+        }
+        av_log(avctx, AV_LOG_DEBUG, "Set max_cll=%s.\n", ctx->max_cll);
+    }
+
+    if (ctx->master_display != 0) {
+        if (ctx->api->param_parse(ctx->params, "master-display", ctx->master_display) == X265_PARAM_BAD_VALUE) {
+            av_log(avctx, AV_LOG_ERROR, "Invalid master_display: %s.\n", ctx->master_display);
+            return AVERROR(EINVAL);
+        }
+        av_log(avctx, AV_LOG_DEBUG, "Set master_display=%s.\n", ctx->master_display);
+    }
+
+    if (ctx->bframes >= 0) {
+        char bframes[20];
+
+        snprintf(bframes, sizeof(bframes), "%d", ctx->bframes);
+        if (ctx->api->param_parse(ctx->params, "bframes", bframes) == X265_PARAM_BAD_VALUE) {
+            av_log(avctx, AV_LOG_ERROR, "Invalid bframes: %d.\n", ctx->bframes);
+            return AVERROR(EINVAL);
+        }
+        av_log(avctx, AV_LOG_DEBUG, "Set bframes=%d.\n", ctx->bframes);
     }
 
     ctx->params->rc.vbvBufferSize = avctx->rc_buffer_size / 1000;
@@ -525,12 +555,15 @@ static av_cold void libx265_encode_init_csp(AVCodec *codec)
 #define OFFSET(x) offsetof(libx265Context, x)
 #define VE AV_OPT_FLAG_VIDEO_PARAM | AV_OPT_FLAG_ENCODING_PARAM
 static const AVOption options[] = {
-    { "crf",         "set the x265 crf",                                                            OFFSET(crf),       AV_OPT_TYPE_FLOAT,  { .dbl = -1 }, -1, FLT_MAX, VE },
-    { "forced-idr",  "if forcing keyframes, force them as IDR frames",                              OFFSET(forced_idr),AV_OPT_TYPE_BOOL,   { .i64 =  0 },  0,       1, VE },
-    { "preset",      "set the x265 preset",                                                         OFFSET(preset),    AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
-    { "tune",        "set the x265 tune parameter",                                                 OFFSET(tune),      AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
-    { "profile",     "set the x265 profile",                                                        OFFSET(profile),   AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
-    { "x265-params", "set the x265 configuration using a :-separated list of key=value parameters", OFFSET(x265_opts), AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
+    { "crf",            "set the x265 crf",                                                            OFFSET(crf),             AV_OPT_TYPE_FLOAT,  { .dbl = -1 }, -1, FLT_MAX, VE },
+    { "max-cll",        "set the x265 max cll,max fall",                                               OFFSET(max_cll),         AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
+    { "master-display", "set the x265 master display",                                                 OFFSET(master_display),  AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
+    { "bframes",        "set max number of consecutive bframes",                                       OFFSET(bframes),         AV_OPT_TYPE_INT,    { .i64 = 0 }, 0, 0, VE },
+    { "forced-idr",     "if forcing keyframes, force them as IDR frames",                              OFFSET(forced_idr),      AV_OPT_TYPE_BOOL,   { .i64 =  0 },  0,       1, VE },
+    { "preset",         "set the x265 preset",                                                         OFFSET(preset),          AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
+    { "tune",           "set the x265 tune parameter",                                                 OFFSET(tune),            AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
+    { "profile",        "set the x265 profile",                                                        OFFSET(profile),         AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
+    { "x265-params",    "set the x265 configuration using a :-separated list of key=value parameters", OFFSET(x265_opts),       AV_OPT_TYPE_STRING, { 0 }, 0, 0, VE },
     { NULL }
 };
 
