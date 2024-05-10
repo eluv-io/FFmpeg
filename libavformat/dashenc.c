f65c176769e869dfe1223df76ae04f02a25c21b9
John Heaton <john.heaton@eluv.io>
2024-05-09 21:38:10 -0400

    Fix SIGFPE in dashenc.c due to total_duration=0

--- libavformat/dashenc.c
+++ libavformat/dashenc.c
@@ -954,7 +954,7 @@ static int write_adaptation_set(AVFormatContext *s, AVIOContext *out, int as_ind
 
         if (os->bit_rate > 0)
             snprintf(bandwidth_str, sizeof(bandwidth_str), " bandwidth=\"%d\"", os->bit_rate);
-        else if (final) {
+        else if (final&& (c->total_duration > 0)) {
             int average_bit_rate = os->pos * 8 * AV_TIME_BASE / c->total_duration;
             snprintf(bandwidth_str, sizeof(bandwidth_str), " bandwidth=\"%d\"", average_bit_rate);
         } else if (os->first_segment_bit_rate > 0)
