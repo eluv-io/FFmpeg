25ea2681c2c81162999cbcd4cc6166363121d6fb
Reza Mir <reza.mir@eluv.io>
2024-11-19 20:04:00 -0500

    Modification of condition

--- libavformat/dashenc.c
+++ libavformat/dashenc.c
@@ -2417,7 +2417,7 @@ static int dash_write_packet(AVFormatContext *s, AVPacket *pkt)
 
     if (s->prev_pts != AV_NOPTS_VALUE) {
         frame_duration_variation = (s->max_frame_duration - s->min_frame_duration) * 2;
-        if (frame_duration_variation > pkt->duration)
+        if (frame_duration_variation >= pkt->duration)
             frame_duration_variation = pkt->duration - 1;
     }
 
