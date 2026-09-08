6d3fffb459cd8d4c670133a1a4eba1e7f83b7016
Jan <jan.falkin@eluv.io>
2026-09-08 20:18:49 +0000

    no uFConvertImage too expensive fail if not RGB

--- libavfilter/vf_uniqfeed.c
+++ libavfilter/vf_uniqfeed.c
@@ -491,10 +491,9 @@ static int create_frame_from_render_image(const UfImage *image,
         return AVERROR_EXTERNAL;
 
     if (format != R8G8B8_UINT) {
-        rgb_image = uFConvertImage(image, R8G8B8_UINT);
-        if (!rgb_image)
-            return AVERROR_EXTERNAL;
-        image = rgb_image;
+        av_log(NULL, AV_LOG_ERROR, "uniqfeed render image format %d is not supported, expected %d\n",
+               format, R8G8B8_UINT);
+        return AVERROR(EINVAL);
     }
 
     if (uFGetImageSize(image, &width, &height) != 0 ||
